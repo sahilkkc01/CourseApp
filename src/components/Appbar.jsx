a3de4d168e5fb984d4556e152264467c15c07fe0
sahilkkc01 <sahilgupta5655@gmail.com>
2024-05-24 12:03:41 +0530

    6

--- src/components/Appbar.jsx
+++ src/components/Appbar.jsx
@@ -54,7 +54,7 @@ export default function Appbar() {
             }}
             variant={"h6"}
           >
-            Alemeno 
+            Alemeno
           </Typography>
         </div>
         <div style={{ display: "flex" }}>
